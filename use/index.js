1bb8849f7e578e76fd31fbd8954d10f8d6def481
wzqwangyoucao <1737481406@qq.com>
2018-09-25 21:14:39 +0800

    use add package commit

---
+++ use/index.js
@@ -0,0 +1,3 @@
+var add = require ('myaddsecond')
+var sum = add.add('123','234')
+console.log(sum);
---
+++ use/node_modules/myaddsecond/index.js
@@ -0,0 +1,5 @@
+exports.add = function(num1, num2) {
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    return num1 + num2
+}
